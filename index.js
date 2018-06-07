963bc0d362e36cfc0572d803dffa6147bfaad00c
kek <oleh.petryk@gmail.com>
2018-06-07 22:33:31 +0300

    Nice commit

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-
 for (let i = 0; i < document.getElementsByClassName('image').length; i++) {
     document.getElementsByClassName('image')[i].style.opacity = '0';
 }
-window.onload = () => {
 setTimeout(() => {
     document.getElementsByClassName('image')[0].style.opacity = '1';
-}, 100);
-};
-let counter = 0;
-setInterval(() => {
-    counter++;
-    if (counter === 5) {
-        document.getElementsByClassName('image')[4].style.opacity = '0';
-        document.getElementsByClassName('image')[0].style.opacity = '1';
-        counter = 0;
-        return;
-    }
-    document.getElementsByClassName('image')[counter - 1].style.opacity = '0';
-    document.getElementsByClassName('image')[counter].style.opacity = '1';
-}, 5000);
+    let counter = 0;
+    setInterval(() => {
+        counter++;
+        if (counter === 5) {
+            document.getElementsByClassName('image')[4].style.opacity = '0';
+            document.getElementsByClassName('image')[0].style.opacity = '1';
+            counter = 0;
+            return;
+        }
+        document.getElementsByClassName('image')[counter - 1].style.opacity = '0';
+        document.getElementsByClassName('image')[counter].style.opacity = '1';
+    }, 5000);
+}, 500);
